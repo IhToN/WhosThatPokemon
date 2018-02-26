63b192b37736eee8d868cfe681e4b7b2ef676a3c
IhToN <ihton93@gmail.com>
2018-02-26 20:28:21 +0100

    Userwon post and method

--- server/Routes.js
+++ server/Routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const formidable = require("formidable");
 
 
+const Pokemon = require('./models/Pokemon');
+const User = require('./models/User');
 
 router.get('*', function (req, res) {
   res.sendfile(path.join(__dirname, 'public', 'index.html'));
@@ -16,7 +18,7 @@ router.post('/upload', function (req, res) {
   form.parse(req);
 
   form.on('fileBegin', function (name, file) {
-    var fileType = file.type.split('/').pop();
+    let fileType = file.type.split('/').pop();
     if (fileType === 'jpg' || fileType === 'png' || fileType === 'jpeg' || fileType === 'gif') {
       file.path = path.join(__dirname, 'public', 'uploads', file.name);
 
@@ -35,32 +37,77 @@ router.post('/pokemon', function (req, res) {
 });
 
 router.post('/register', function (req, res) {
-  //todo: create a new user if not registered
-  if (req.body.email &&
-    req.body.username &&
-    req.body.password &&
-    req.body.passwordConf) {
+    //todo: create a new user if not registered
+    if (req.body.uuid &&
+      req.body.name &&
+      req.body.password &&
+      req.body.passwordConf &&
+      req.body.color &&
+      req.body.avatar) {
+      if (req.body.password === req.body.passwordConf) {
 
-    var userData = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-      passwordConf: req.body.passwordConf,
-    };
+        const userData = {
+          uuid: req.body.uuid,
+          name: req.body.name,
+          password: req.body.password,
+          color: req.body.color,
+          avatar: req.body.avatar,
+          wins: 1,
+        };
 
-    //use schema.create to insert data into the db
-    User.create(userData, function (err, user) {
-      if (err) {
-        return next(err)
+        //use schema.create to insert data into the db
+        User.create(userData, function (err, user) {
+          if (err) {
+            return next(err)
+          } else {
+            req.session.userId = user._id;
+            //todo: tell the user that has been registered
+          }
+        });
       } else {
-        return res.redirect('/profile');
+        return next('Las contraseñas no son las mismas.');
+      }
+    } else {
+      let err = new Error('No has rellenado los campos necesarios.');
+      err.status = 400;
+      return next(err);
+    }
+  }
+);
+
+router.post('/login', function (req, res) {
+  if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        let err = new Error('La combinación de usuario y contraseña no se ha podido encontrar.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        //todo: tell the user that has been logged
       }
     });
+  } else {
+    let err = new Error('No has rellenado los campos necesarios.');
+    err.status = 400;
+    return next(err);
   }
 });
 
-router.post('/login', function (req, res) {
-  //todo: login new user if registered
+router.post('/userwon', function (req, res) {
+  if (req.body.uuid) {
+    User.addWin(req.body.uuid, function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return next('Usuario actualizado');
+      }
+    });
+  } else {
+    let err = new Error('No has introducido una UUID.');
+    err.status = 400;
+    return next(err);
+  }
 });
 
 module.exports = router;
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 /*
     uuid: '0000-0000-0000',
@@ -9,13 +10,63 @@ const mongoose = require('mongoose');
     wins: 0
  */
 
-const User = mongoose.model('User', new mongoose.Schema({
-  uuid: {type: String, required: true, unique: true},
-  name: {type: String, required: true},
+const UserSchema = new mongoose.Schema({
+  uuid: {type: String, required: true, unique: true, trim: true},
+  name: {type: String, required: true, trim: true},
   password: {type: String, required: true},
-  color: {type: String, required: true},
-  avatar: {type: String, required: true},
+  // passwordConf: {type: String, required: true},
+  color: {type: String, required: true, trim: true},
+  avatar: {type: String, required: true, trim: true},
   wins: Number
-}, {retainKeyOrder: true}));
+}, {retainKeyOrder: true});
 
+UserSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({name: username})
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err);
+      } else if (!user) {
+        err = new Error('Usuario no encontrado');
+        err.status = 401;
+        return callback(err);
+      } else {
+        bcrypt.compare(password, user.password, function (err, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        });
+      }
+    });
+};
+
+UserSchema.statics.addWin = function (uuid, callback) {
+  User.findOneAndUpdate({uuid: uuid}, {$inc: {wins: 1}},
+    function (err, user) {
+      if (err) {
+        return callback(err);
+      } else if (!user) {
+        err = new Error('Usuario no encontrado');
+        err.status = 401;
+        return callback(err);
+      } else {
+        console.log('Usuario actualizado');
+      }
+    });
+};
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
